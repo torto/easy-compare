b675097d9540fd603d90fd8793d6fa5c075fb859
Guilherme Berghauser <tortoyoyo@gmail.com>
2019-08-19 11:36:30 +0100

    Adjust npmignore and add minify

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// const webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
